d6710e43b6b763a1f58de55fab26f7e5f2e56ff8
Adam Anderson <AdamAnderson9376@gmail.com>
2023-03-17 13:19:54 -0600

    Fixes #1 Corrected number of repeated words

--- word-repeater.py
+++ word-repeater.py
@@ -4,7 +4,7 @@ def main():
     """
     word = input("Enter word to repeat: ")
     n = int(input("Enter number of times to repeat: "))
-
+#al;sdkjaf;
     for i in range(1, (n+1)):
         print(word)
 
